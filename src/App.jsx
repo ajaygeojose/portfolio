4db586dd3c7a2792945410d212c2ad1d10f4d482
ajaygeojose <ajaygeojose@gmail.com>
2025-09-16 18:30:55 +0100

    added mobile view to side menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
@@ -6,16 +6,48 @@ import Skillset from './components/skillset/Skillset'
 import Navbar from './components/navbar/Navbar'
 import Sidepanel from './components/sidepanel/sidepanel'
 import ContentPanel from './components/contentPanel/contentPanel'
+import { Button, Drawer } from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 
 function App() {
   const navs=["Who a'm I","Career Objectives","Projects","Social Media"]
+  const [open, setOpen] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+  const toggleDrawer = () => () => {
+    setOpen((prev) => !prev);
+  };
+  function handleWindowSizeChange() {
+      setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+      window.addEventListener('resize', handleWindowSizeChange);
+      return () => {
+          window.removeEventListener('resize', handleWindowSizeChange);
+      }
+  }, []);
+  const isMobile = width <= 768;
+  const Ham=()=>{
+    return(<Button onClick={toggleDrawer(true)}><MenuIcon/></Button>)
+  }
+  const Close=()=>{
+    return(<Button onClick={toggleDrawer(false)}><CloseOutlinedIcon/></Button>)
+  }
   return (
     <div className="app">
       <div className='header'>
          <Header/>
       </div>
+      {isMobile && <Ham/>}
       <div className='midLayer'>
+        {!isMobile && <Sidepanel/> }
+        
+      <Drawer  open={open} onClose={toggleDrawer(false)}>
+       <Close/>
         <Sidepanel/>
+      </Drawer>
+
+        {/* <Sidepanel/> */}
         <ContentPanel/>
       </div>
       
