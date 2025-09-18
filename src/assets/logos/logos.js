7163f8b3d8ffd7aae14577de8d28ac249e99cdf6
ajaygeojose <ajaygeojose@gmail.com>
2025-09-18 18:15:59 +0100

    updated image paths

--- src/assets/logos/logos.js
+++ src/assets/logos/logos.js
@@ -1,3 +1,3 @@
-export const riskcovryLogo = "src/assets/logos/Riskcovry-logo.png"
-export const questLogo = "src/assets/logos/quest-global-logo.png";
-export const ontashLogo = "src/assets/logos/Ontash-logo.png";
+export const riskcovryLogo = "./src/assets/logos/Riskcovry-logo.png"
+export const questLogo = "./src/assets/logos/quest-global-logo.png";
+export const ontashLogo = "./src/assets/logos/Ontash-logo.png";
